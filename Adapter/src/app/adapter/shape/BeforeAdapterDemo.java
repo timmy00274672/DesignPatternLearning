2a8931dadbb7409245ef037ccf9fe8726c833ff3
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-21 14:42:56 +0800

    Add another example to demo adapter pattern.

--- Adapter/src/app/adapter/shape/BeforeAdapterDemo.java
+++ Adapter/src/app/adapter/shape/BeforeAdapterDemo.java
@@ -1,4 +1,4 @@
-package app.adapter;
+package app.adapter.shape;
 
 class LegacyLine {
     public void draw(int x1, int y1, int x2, int y2) {
@@ -16,14 +16,16 @@ class LegacyRectangle {
 
 public class BeforeAdapterDemo {
     public static void main(String[] args) {
+	System.out.println("HI");
 	Object[] shapes = { new LegacyLine(), new LegacyRectangle() };
 	// A begin and end point from a graphical editor
 	int x1 = 10, y1 = 20;
 	int x2 = 30, y2 = 60;
 	for (int i = 0; i < shapes.length; ++i)
-	    if (shapes[i].getClass().getName().equals("LegacyLine"))
+	    if (shapes[i].getClass().getSimpleName().equals("LegacyLine"))
 		((LegacyLine) shapes[i]).draw(x1, y1, x2, y2);
-	    else if (shapes[i].getClass().getName().equals("LegacyRectangle"))
+	    else if (shapes[i].getClass().getSimpleName()
+		    .equals("LegacyRectangle"))
 		((LegacyRectangle) shapes[i]).draw(Math.min(x1, x2),
 			Math.min(y1, y2), Math.abs(x2 - x1), Math.abs(y2 - y1));
     }
---
+++ Adapter/src/app/adapter/shape/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * It's the 
+ * <a href="http://sourcemaking.com/design_patterns/adapter/java/1">
+ * 	reference
+ * </a>.
+ */
+package app.adapter.shape;
